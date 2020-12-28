3988e234fb5a24706d6eefeb02b20a6180afef68
blacklantern047 <aryan.bhardwaj47@gmail.com>
2020-12-28 20:04:09 +0530

    first commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { ApplicationConfig, ChromexApplication } from './application';
+export * from './application';
+export declare function main(options?: ApplicationConfig): Promise<ChromexApplication>;
