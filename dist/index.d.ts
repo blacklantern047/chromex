e48b22fce3e3ebd31ec607373db3ffc0097aaec1
blacklantern047 <aryan.bhardwaj47@gmail.com>
2020-12-30 12:00:26 +0530

    removed api explorer

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, ChromexApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<ChromexApplication>;
