aa7800928d1c280bf30473c9fc8b8ccb5b31afb2
blacklantern047 <aryan.bhardwaj47@gmail.com>
2021-01-10 13:57:23 +0530

    minor

--- src/controllers/chromex.controller.ts
+++ src/controllers/chromex.controller.ts
@@ -6,42 +6,21 @@ export class GoCarReviewController {
     }
     @get('chromex/wallpaper')
     async wallpaper(){
-        let finalResponse: any;
-        await axios.get("https://wall.alphacoders.com/api2.0/get.php?auth=1e3c6bcb559b151f8ddde6299f755938&method=category&id=3")
+        let finalUrl: string = "https://images5.alphacoders.com/587/587597.jpg";
+        let randomNumber: number = Math.floor(Math.random() * 184980) + 1;
+        let pageNumber: number = randomNumber/30 + 1;
+        let wallpaperNumber = Math.floor(Math.random() * 30) + 1;
+        await axios.get(`https://wall.alphacoders.com/api2.0/get.php?auth=1e3c6bcb559b151f8ddde6299f755938&method=category&id=3&page=${pageNumber}`)
             .then(function (response: any) {
                 // handle success
-                finalResponse = response.data;
+                if(response.data && response.data.success && response.data.wallpapers && response.data.wallpapers[wallpaperNumber] && response.data.wallpapers[wallpaperNumber].url_image){
+                    finalUrl = response.data.wallpapers[wallpaperNumber].url_image;
+                }
             })
             .catch(function (error: any) {
                 // handle error
                 console.log(error);
             })
-        return finalResponse;
-        // let url = "https://wall.alphacoders.com/api2.0/get.php?auth=1e3c6bcb559b151f8ddde6299f755938&method=category&id=3";
-        // let requestParams: any = {url: url, json: true, time: true, timeout: 1000, pool: {maxSockets: 100}};
-        // let apiResponse: any = (requestParams: any) => {
-        //     return new Promise(
-        //         (resolve, reject) => {
-        //             request.get(requestParams, function (error: any, response: any, responseJson:any) {
-        //                 if(error || !response || response.statusCode !== 200) {
-        //                     console.log(error);
-        //                     let err = new Error();
-        //                     err.message = "Error from wallpapers api";
-        //                     resolve({"status": false, "error": err});
-        //                 }
-        //                 else if (!responseJson) {
-        //                     let err= new Error();
-        //                     err.message = "Not a valid json";
-        //                     resolve({"status": false, "error": err});
-        //                 }
-        //                 else{
-        //                     resolve(responseJson);
-        //                 }
-        //             })
-        //         }
-        //     );
-        // };
-        // let responseObj: any = await apiResponse(requestParams);
-        // return responseObj;
+        return finalUrl;
     }
 }
